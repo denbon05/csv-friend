9b641419431048d1135dc2a71eb9192e87275b16
denbon05 <d.bondarenko@tme.pl>
2023-12-12 08:43:22 +0100

    Improve DX

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,6 +1,12 @@
 export interface IOptions {
+  /**
+   * Should headers be attached.
+   * @default true
+   * */
   headers: boolean;
+  /** @default , */
   delimiter: string;
+  /** @default true */
   isEmptyIfNullable: boolean;
 }
 
